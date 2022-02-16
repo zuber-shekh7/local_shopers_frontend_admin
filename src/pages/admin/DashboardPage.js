2cf2d1daf04b530dfbe78294eceb5bec4ce4567f
Zuber Shekh <zuber.shekh7@gmail.com>
2022-02-16 19:03:37 +0530

    add icons to the dashboard menu items

--- src/pages/admin/DashboardPage.js
+++ src/pages/admin/DashboardPage.js
@@ -1,6 +1,13 @@
 import React from "react";
 import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
+import {
+  HiOutlineHome,
+  HiOutlineUsers,
+  HiOutlineViewGrid,
+  HiOutlineQuestionMarkCircle,
+  HiOutlineMenu,
+} from "react-icons/hi";
 
 const DashboardPage = () => {
   const { user } = useSelector((state) => state.adminLogin);
@@ -10,49 +17,49 @@ const DashboardPage = () => {
         {/* dashboard menu */}
         <div className="bg-indigo-500 w-72 h-screen">
           <div className="flex items-center bg-indigo-700">
-            <h1 className="text-2xl font-bold text-white p-5">
-              Local Shoppers
+            <h1 className="flex items-center text-2xl font-bold text-white p-5">
+              <HiOutlineMenu className="mr-2" /> Menu
             </h1>
           </div>
           <div>
             <div className="px-5 py-4 hover:bg-indigo-600">
               <Link
-                className="text-xl font-medium text-white"
+                className="text-xl font-medium text-white flex items-center gap-2"
                 to="/admin/account"
               >
-                Home
+                <HiOutlineHome className="inline h-6 w-6" /> Home
               </Link>
             </div>
             <div className="px-5 py-4 hover:bg-indigo-600">
               <Link
-                className="text-xl font-medium text-white"
+                className="text-xl font-medium text-white flex items-center gap-2"
                 to="/admin/account"
               >
-                Sellers
+                <HiOutlineUsers className="inline h-6 w-6" /> Customers
               </Link>
             </div>
             <div className="px-5 py-4 hover:bg-indigo-600">
               <Link
-                className="text-xl font-medium text-white"
+                className="text-xl font-medium text-white flex items-center gap-2"
                 to="/admin/account"
               >
-                Customers
+                <HiOutlineUsers className="inline h-6 w-6" /> Sellers
               </Link>
             </div>
             <div className="px-5 py-4 hover:bg-indigo-600">
               <Link
-                className="text-xl font-medium text-white"
+                className="text-xl font-medium text-white flex items-center gap-2"
                 to="/admin/account"
               >
-                Categories
+                <HiOutlineViewGrid className="inline h-6 w-6" /> Categories
               </Link>
             </div>
             <div className="px-5 py-4 hover:bg-indigo-600">
               <Link
-                className="text-xl font-medium text-white"
+                className="text-xl font-medium text-white flex items-center gap-2"
                 to="/admin/account"
               >
-                Help
+                <HiOutlineQuestionMarkCircle className="inline h-6 w-6" /> Help
               </Link>
             </div>
           </div>
